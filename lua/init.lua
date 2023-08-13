caf34b8bf6f5d5ef2a4d84de45b52db0ed35b1f0
pysnow530 <pysnow530@163.com>
2023-08-13 14:05:58 +0800

    add nvim-treesitter

--- lua/init.lua
+++ lua/init.lua
@@ -76,6 +76,27 @@ require("lazy").setup({
     -- ai
     "github/copilot.vim",
 
+    -- languages: general
+    {
+        'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "javascript", "html", "c_sharp", },
+
+                highlight = {
+                    enable = true,
+                },
+
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            }
+        end
+    },
+
     -- languages: javascript
     "pangloss/vim-javascript",
     "mxw/vim-jsx",
