dc38c05840ac89f9511d2457ac6629470f08f0c1
oogwu <oogwu@tencent.com>
2024-10-15 11:19:46 +0800

    fix: 修复nvim-cmp配置

--- lua/plugins/lang.lsp.lua
+++ lua/plugins/lang.lsp.lua
@@ -96,7 +96,14 @@ return {
             -- Set up lspconfig.
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-            require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+
+            require('lspconfig')['pyright'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['lua_ls'].setup {
+                capabilities = capabilities
+            }
+            require('lspconfig')['ts_ls'].setup {
                 capabilities = capabilities
             }
         end
