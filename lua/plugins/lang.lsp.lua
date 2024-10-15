db7f37f72059523c3636efeb7279f08cae1a74ed
oogwu <oogwu@tencent.com>
2024-10-15 14:52:55 +0800

    feat: lua lsp指定为neovim使用

--- lua/plugins/lang.lsp.lua
+++ lua/plugins/lang.lsp.lua
@@ -4,13 +4,37 @@ return {
         config = function()
             -- :help lspconfig-all
             require'lspconfig'.pyright.setup{}
-            require'lspconfig'.lua_ls.setup{
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' }
+            require'lspconfig'.lua_ls.setup {
+                on_init = function(client)
+                    if client.workspace_folders then
+                        local path = client.workspace_folders[1].name
+                        if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+                            return
+                        end
+                    end
+
+                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT'
+                        },
+                        -- Make the server aware of Neovim runtime files
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME
+                                -- Depending on the usage, you might want to add additional paths here.
+                                -- "${3rd}/luv/library"
+                                -- "${3rd}/busted/library",
+                            }
+                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                            -- library = vim.api.nvim_get_runtime_file("", true)
                         }
-                    }
+                    })
+                end,
+                settings = {
+                    Lua = {}
                 }
             }
             require'lspconfig'.ts_ls.setup{}
@@ -97,15 +121,12 @@ return {
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
-            require('lspconfig')['pyright'].setup {
-                capabilities = capabilities
-            }
-            require('lspconfig')['lua_ls'].setup {
-                capabilities = capabilities
-            }
-            require('lspconfig')['ts_ls'].setup {
-                capabilities = capabilities
-            }
+            local servers = {'pyright', 'ts_ls'}
+            for _, lsp in ipairs(servers) do
+                require('lspconfig')[lsp].setup {
+                    capabilities = capabilities
+                }
+            end
         end
     },
 }
