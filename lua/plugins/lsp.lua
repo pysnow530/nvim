74df0a446cb1bac81742f15ddcf348f147648f96
oogwu <oogwu@tencent.com>
2024-10-17 14:33:34 +0800

    fix: nvim-cmp补全缺失的依赖

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip',
         },
         config = function()
             -- Set up nvim-cmp.
