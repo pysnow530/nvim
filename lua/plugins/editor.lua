19844956c505212bb1434be5bf8126ca31291fc3
oogwu <oogwu@tencent.com>
2024-10-17 11:08:01 +0800

    feat: 支持默认grep当前cword

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,28 +1,44 @@
 -- ripgrep
+local function find_git_root_dir()
+    -- current path
+    local curr_buf_path = vim.fn.expand('%:p')
+    if curr_buf_path == nil or curr_buf_path == "" then
+        error(string.format('Expand current path error: %q', curr_buf_path))
+    end
+
+    -- git root directory
+    local util = require('lspconfig.util')
+    local root_dir = util.find_git_ancestor(curr_buf_path)
+    if root_dir == nil then
+        error(string.format('Find git ancestor error: %q', curr_buf_path))
+    end
+
+    return root_dir
+end
 vim.opt.grepprg = 'rg --vimgrep $*'
 vim.api.nvim_create_user_command('RipGrep', "execute 'silent grep! <args>' | redraw! | copen 10", { nargs = 1 })
 vim.api.nvim_set_keymap('n', '<leader>g', '', {
-    callback =
-        function()
-            -- current path
-            local curr_buf_path = vim.fn.expand('%:p')
-            if curr_buf_path == nil or curr_buf_path == "" then
-                error(string.format('Expand current path error: %q', curr_buf_path))
-            end
-
-            -- git root directory
-            local util = require('lspconfig.util')
-            local root_dir = util.find_git_ancestor(curr_buf_path)
-            if root_dir == nil then
-                error(string.format('Find git ancestor error: %q', curr_buf_path))
-            end
-
-            local search = vim.fn.input('rg search: ', vim.fn.expand('<cword>'))
-            if search ~= "" then
-                vim.cmd(string.format(':RipGrep %q %q', search, root_dir))
-            end
+    callback = function()
+        local root_dir = find_git_root_dir()
+        local pattern = vim.fn.expand('<cword>')
+        if string.len(pattern) < 3 then
+            error('Pattern on curser is too short: ' .. pattern)
         end
-    ,
+
+        vim.cmd(string.format(':RipGrep %q %q', pattern, root_dir))
+    end,
+    desc = 'Ripgrep search words'
+})
+vim.api.nvim_set_keymap('n', '<leader><leader>g', '', {
+    callback = function()
+        local root_dir = find_git_root_dir()
+        local pattern = vim.fn.input('rg pattern: ')
+        if pattern == "" then
+            return
+        end
+
+        vim.cmd(string.format(':RipGrep %q %q', pattern, root_dir))
+    end,
     desc = 'Ripgrep search words'
 })
 
